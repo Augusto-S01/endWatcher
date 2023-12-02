c4b11c049c08b66c37ccf43736d59222d81377e9
Augusto Souza <augustosouzas01@gmail.com>
2023-12-02 01:35:25 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron')
+
+let win;
+
+function createWindow () {
+  win = new BrowserWindow(
+    {
+      width: 800,
+      height: 600,
+      backgroundColor: '#ffffff',
+    }
+  )
+
+win.loadURL(`http://google.com`);
+win.on(`closed`, function () {
+  win = null
+  })
+}
+
+
+app.on(`ready`, createWindow)
+
+app.on(`window-all-closed`, function () {
+  if (process.platform !== `darwin`) {
+    app.quit()
+  }
+}
+)
+
+
