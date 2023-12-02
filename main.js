5c75de41378e7af2c4d93adf7abeb9d5145f918b
Augusto Souza <augustosouzas01@gmail.com>
2023-12-02 02:51:06 -0300

    creating scss reset global

--- main.js
+++ main.js
@@ -1,30 +1,25 @@
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require("electron");
 
 let win;
 
-function createWindow () {
-	win = new BrowserWindow(
-		{
-			width: 800,
-			height: 600,
-			backgroundColor: "#ffffff",
-		}
-	);
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: "#ffffff",
+    closable: false,
+  });
 
-	win.loadURL("http://google.com");
-	win.on("closed", function () {
-		win = null;
-	});
+  win.loadURL("http://google.com");
+  win.on("closed", function () {
+    win = null;
+  });
 }
-
 
 app.on("ready", createWindow);
 
 app.on("window-all-closed", function () {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
-}
-);
-
-
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
